f12efcbb975fa7dd389c0aaf8755d84b804e79b6
davidpodhola <david.podhola@naseukoly.cz>
2017-05-02 23:05:33 +0200

    really passing JSON arround, for now only as untyped in Rust (https://github.com/serde-rs/json#operating-on-untyped-json-values)

--- fable/library.fsx
+++ fable/library.fsx
@@ -14,6 +14,19 @@ module Util =
         Browser.localStorage.setItem(key, JS.JSON.stringify data)
         
 open Fable.Import.Browser        
+
+type Tree =
+    | Leaf of int
+    | Branch of Tree[]
+    member this.Sum() =
+        match this with
+        | Leaf i -> i
+        | Branch trees ->
+            trees |> Seq.map (fun x -> x.Sum()) |> Seq.sum
+
+let tree =
+    Branch [|Leaf 1; Leaf 2; Branch [|Leaf 3; Leaf 4|]|]
+
 let fableFunc() = 
     console.log( "Fable code invoked again" )
     System.DateTime.Now |> Util.save "current-time"
@@ -22,5 +35,5 @@ let fableFunc() =
 let fableFuncS() = 
     console.log( "Fable code invoked again" )
     System.DateTime.Now |> Util.save "current-time"
-    "Hello World!"
+    toJson tree
 
