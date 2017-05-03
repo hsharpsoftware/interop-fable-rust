0c74608c6e98f0d00bba5877d554bec256b7c5b2
davidpodhola <david.podhola@naseukoly.cz>
2017-05-03 22:30:56 +0200

    does not work

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,67 @@
-var path = require("path");
-var webpack = require("webpack");
+const { resolve } = require('path');
+const webpack = require('webpack');
 
-var cfg = {
-  devtool: "source-map",
-  entry: "./temp/library.js",
+module.exports = {
+  context: resolve(__dirname, 'temp'),
+
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './library.js'
+    // the entry point of our app
+  ],
   output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-	// export itself to a global var
-	libraryTarget: "var",
-	// name of the global var: "Foo"
-	library: "FableLib"	
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'public'),
+
+    publicPath: '/',
+    // necessary for HMR to know where to load the hot update chunks
+
+    // export itself to a global var
+    libraryTarget: "var",
+    // name of the global var: "Foo"
+    library: "FableLib"    
   },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: resolve(__dirname, 'public'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        //exclude: /node_modules/,
-        loader: "source-map-loader"
-      },
-      {
-        test: /\.js$/,
-        //exclude: /node_modules/,
-        loader: "babel-loader"
+        use: [ 'babel-loader', ],
+        exclude: /node_modules/
       }
-    ]
-  }  
-};
+    ],
+  },
 
-module.exports = cfg;
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ],
+};
